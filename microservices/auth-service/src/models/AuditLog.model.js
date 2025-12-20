e31df14eea7894b8288e7ea494c5a1da8f9e460e
Rudraksh Singh <rudrakshsin3006@gmail.com>
2025-12-20 13:11:24 +0530

    auth-service: add AuditLog model stub to satisfy imports

---
+++ microservices/auth-service/src/models/AuditLog.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const AuditLogSchema = new mongoose.Schema(
+  {
+    action: { type: String },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    metadata: { type: Object },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('AuditLog', AuditLogSchema);
+

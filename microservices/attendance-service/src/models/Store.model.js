b6fbb49c7b5998fa2cb08277a5646b0ef2d9dba1
Rudraksh Singh <rudrakshsin3006@gmail.com>
2025-12-20 13:15:56 +0530

    attendance-service: remove duplicate Store schema declaration

--- microservices/attendance-service/src/models/Store.model.js
+++ microservices/attendance-service/src/models/Store.model.js
@@ -10,16 +10,4 @@ const StoreSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model('Store', StoreSchema);
-const mongoose = require('mongoose');
-
-const StoreSchema = new mongoose.Schema(
-  {
-    name: { type: String },
-    code: { type: String },
-    location: { type: String },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model('Store', StoreSchema);
 

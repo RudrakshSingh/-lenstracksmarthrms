4e28b43f66212d295d8973b061994572b0fd1beb
Rudraksh Singh <rudrakshsin3006@gmail.com>
2025-12-30 15:00:32 +0530

    fix: Remove entire duplicate User model definition

--- microservices/attendance-service/src/models/User.model.js
+++ microservices/attendance-service/src/models/User.model.js
@@ -10,9 +10,6 @@ const UserSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model('User', UserSchema);
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema(
   {
     email: { type: String },
     name: { type: String },
